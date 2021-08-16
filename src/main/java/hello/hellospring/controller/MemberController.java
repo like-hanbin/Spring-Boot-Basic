dbec95aead7290a11db72f5b519aa488a7cb920a
hanbin <likehanbin@gmail.com>
2021-08-16 19:00:02 +0900

    make CR, used static memory

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -10,6 +16,28 @@ public class MemberController {
 
     @Autowired
     public MemberController(MemberService memberService) {
-        MemberService = memberService;
+        this.MemberService = memberService;
     }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+        MemberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = MemberService.findMembers();
+        model.addAttribute("members",members);
+        return "members/memberList";
+    }
+
 }
